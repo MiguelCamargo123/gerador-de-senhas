712e59882fa1efddf1b678281ea2678f8406ab1f
Miguel Camargo <omiguelcamargo@gmail.com>
2026-02-02 17:19:55 -0300

    Variaveis comentadas e loop adicionado

--- gerador_senhas.py
+++ gerador_senhas.py
@@ -1,8 +1,14 @@
 import random
 
-numeros = random.randrange(1, 6)
+# numeros = random.randint(1, 6)
 
-resultado = str(numeros)
-
+# resultado = str(numeros)
 senha = []
+
+# senha.append(resultado)
+
+for i in range(6):
+    a = random.randrange(1, 6)
+    senha.append(a)
+print(senha)
 
